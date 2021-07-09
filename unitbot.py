cece80f9f98d02d81ddd6913a891d47bd1e60756
Ethan Ruszanowski <ethan@rusz.dev>
2021-07-09 12:54:41 -0400

    Update correctionText to use nicknames

--- unitbot.py
+++ unitbot.py
@@ -44,7 +44,7 @@ async def on_message(message): # Catches send messages and corrects non-SI units
     if bot.user.id is not message.author.id and message.author.bot is False and (message.guild is None or (message.guild is not None and discord.utils.get(message.guild.roles, name='imperial certified') not in message.author.roles)):
         processedMessage = unitconversion.process(message.content)
         if processedMessage is not None:
-            correctionText = ("I think " + (message.author.name if message.guild is not None else "you") + " meant to say: ```" + processedMessage + "```")
+            correctionText = ("I think " + (message.author.display_name if message.guild is not None else "you") + " meant to say: ```" + processedMessage + "```")
             await message.channel.send(correctionText)
     await bot.process_commands(message)
 

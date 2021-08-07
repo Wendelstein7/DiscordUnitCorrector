caffc619d4bd77eaf5597ebf8adf8ce864db5ccf
Randall Scharpf <randall.scharpf@gmail.com>
2021-08-06 20:27:21 -0700

    most of the way there! see the three todos

--- unitconversion.py
+++ unitconversion.py
@@ -37,6 +37,7 @@ NUMBER_UNIT_SPACERS_RGX = re.compile("\\s*$")
 END_NUMBER_RGXS = []
 for rgx in NUMBER_RGX_STRS:
     END_NUMBER_RGXS.append((re.compile(rgx[0]+"$"), rgx[1], rgx[2]))
+SUPERUNIT_SUBUNIT_SPACER_RGX = re.compile("\\s*$") # TODO make this more effective
 REMOVE_REGEX = re.compile("((´|`)+[^>]+(´|`)+)")
 FORMAT_CONTROL_REGEX = re.compile("(?<!\\\\)(´|`|\\*|_|~~)|((?<=\n)> |(?<=^)> )")
 
@@ -109,6 +110,9 @@ class Unit:
     def getName( self ):
         return self._friendlyName
 
+    def __str__(self):
+        return self._friendlyName
+
     @abstractmethod
     def convert( self, message ): pass
 
@@ -128,66 +132,80 @@ def readNumFromStringEnd( string ):
         cleanedstring = bestrgx[2].sub(".", cleanedstring)
         return (SigFigCompliantNumber(cleanedstring), string[0 : numberResult.start()])
 
-def convertUnitInModificableMessage( message, unit_regex, toMetric ):
-    originalText = message.getText()
-    iterator = unit_regex.finditer( originalText )
-    replacements = []
-    for find in iterator:
-        preunitstr = originalText[ 0 : find.start() ]
-        spacerRes = NUMBER_UNIT_SPACERS_RGX.search(preunitstr)
-        if spacerRes is None:
-            continue
-        SPACING = spacerRes.group()
-        preunitstr = preunitstr[ 0 : spacerRes.start() ]
-        read = readNumFromStringEnd(preunitstr)
-        if read is None:
-            continue
-        (usernumber, preunitstr) = read
-        metricValue = toMetric( usernumber, SPACING )
-        if metricValue is None:
-            continue
-        # get all superunits of this found unit
-        #   a superunit is any bigger unit of the same type, where both units are proportional units when compared with the SI
-        # check if each superunit terminates "preunitstr"
-        #   to do this, each unit should have a generated list of its superunits
-        #   and each of those units should have a different compiled regex from the non-super version of that unit
-        #   specifically, it has an extra lookahead for the end of the string `(?=$)` tacked on
-        # if a superunit is found, repeat the procedure from line 136 to 165 on that unit, including this procedure
-        #   :O recursion :O insert random meme
-        # then convert one unit to the other, so that they're all in one unit, and determine the correct number of sig figs
-        # collect the different strings used by the different units as SPACING and determine a compromise output spacing
-        # perform the replacement with the final, computed value
-        # edge cases are best dealt with outside of this in separate regex-searching procedures or in special superunit definitions
-        repl = {}
-        repl[ "start" ] = len(preunitstr)
-        repl[ "text"  ] = metricValue
-        repl[ "end" ] = find.end()
-        replacements.append(repl)
-    if len(replacements)>0:
-        lastPoint = 0
-        finalMessage = ""
-        for repl in replacements:
-            finalMessage += originalText[ lastPoint: repl[ "start" ] ] + repl[ "text" ]
-            lastPoint = repl["end"]
-        finalMessage += originalText[ lastPoint : ]
-        message.setText(finalMessage)
+def compromiseBetweenStrings( stringarr ):
+    return stringarr[0] # TODO actually implement, this is a dummy.
 
 #NormalUnit class, that follow number + unit name.
 class NormalUnit( Unit ):
-    def __init__( self, friendlyName, regex, unitType, toSIMultiplication, toSIAddition = 0 ):
+    def __init__( self, friendlyName, regex, unitType, toSIMultiplication, toSIAddition = 0, key = None ):
         super( NormalUnit, self ).__init__( friendlyName, unitType, toSIMultiplication, toSIAddition )
-        self._regex = re.compile( "(" + regex + ")((?=$)|(?=[^a-zA-Z]))", re.IGNORECASE )
+        self._regex = re.compile( "(" + regex + ")((?=$)|(?=[^a-z]))", re.IGNORECASE )
+        if key is None:
+            self._key = self
+        else:
+            self._key = key
 
     def convert( self, message ):
-        convertUnitInModificableMessage( message, self._regex, self.toMetric )
-
-class CaseSensitiveUnit( Unit ):
-    def __init__( self, friendlyName, regex, unitType, toSIMultiplication, toSIAddition = 0 ):
-        super( CaseSensitiveUnit, self ).__init__( friendlyName, unitType, toSIMultiplication, toSIAddition )
-        self._regex = re.compile( "(" + regex + ")((?=$)|(?=[^a-zA-Z]))" )
+        originalText = message.getText()
+        iterator = self._regex.finditer( originalText )
+        replacements = []
+        for find in iterator:
+            preunitstr = originalText[ 0 : find.start() ]
+            value = self.getValueFromIteration(preunitstr)
+            if value is None:
+                continue
+            (preunitstr, usernumber, SPACINGS) = value
+            metricValue = self.toMetric(usernumber, compromiseBetweenStrings(SPACINGS))
+            repl = {}
+            repl[ "start" ] = len(preunitstr)
+            repl[ "text"  ] = metricValue
+            repl[ "end" ] = find.end()
+            replacements.append(repl)
+        if len(replacements)>0:
+            lastPoint = 0
+            finalMessage = ""
+            for repl in replacements:
+                finalMessage += originalText[ lastPoint: repl[ "start" ] ] + repl[ "text" ]
+                lastPoint = repl["end"]
+            finalMessage += originalText[ lastPoint : ]
+            message.setText(finalMessage)
     
-    def convert( self, message ):
-        return convertUnitInModificableMessage( message, self._regex, self.toMetric)
+    def getValueFromIteration(self, string, SPACINGS = []):
+        spacerRes = NUMBER_UNIT_SPACERS_RGX.search(string)
+        if spacerRes is None:
+            return None
+        SPACINGS.append(spacerRes.group())
+        preunitstr = string[ 0 : spacerRes.start() ]
+        read = readNumFromStringEnd(preunitstr)
+        if read is None:
+            return None
+        (usernumber, preunitstr) = read
+        nextunitstr = preunitstr[0:SUPERUNIT_SUBUNIT_SPACER_RGX.search(preunitstr).start()]
+        for superunit in superunits[self._key]:
+            match = superunit[0]._regex.search(nextunitstr)
+            if match is None:
+                continue
+            value = superunit[0].getValueFromIteration(preunitstr[0:match.start()], SPACINGS)
+            if value is None:
+                continue
+            (preunitstr, supernumber, SPACINGS) = value
+            ratio = superunit[1]
+            print("will add", supernumber * ratio, "imperial units to the converted value")
+            # combine supernumber and usernumber into a new value for usernumber
+            # gotta make SURE the ratio is cancelled out tho (that is, the value returned is in the subunit, not the superunit)
+            # TODO implement
+            break
+        return (preunitstr, usernumber, SPACINGS)
+
+class CaseSensitiveNormalUnit( NormalUnit ):
+    def __init__( self, friendlyName, regex, unitType, toSIMultiplication, toSIAddition = 0, key = None ):
+        super( CaseSensitiveNormalUnit, self ).__init__( friendlyName, "(placeholder)", unitType, toSIMultiplication, toSIAddition, key )
+        self._regex = re.compile( "(" + regex + ")((?=$)|(?=[^a-zA-Z]))" )
+
+class RegexFlagsExposedNormalUnit( NormalUnit ):
+    def __init__( self, friendlyName, regex, flags, unitType, toSIMultiplication, toSIAddition = 0, key = None ):
+        super( RegexFlagsExposedNormalUnit, self ).__init__( friendlyName, "(placeholder)", unitType, toSIMultiplication, toSIAddition, key )
+        self._regex = re.compile( "(" + regex + ")((?=$)|(?=[^a-zA-Z]))", flags )
 
 # Class containing a string, for the modificable message, and a boolean
 # to indicate if the message has been modified
@@ -211,39 +229,38 @@ units = []
 
 #Area
 units.append( NormalUnit( "inch squared", "in(ch(es)?)? ?(\^2|squared|²)", AREA, 0.00064516 ) )     #inch squared
-units.append( NormalUnit( "foot squared", "f(oo|ee)?t ?(\^2|squared|²)", AREA, 0.092903 ) )         #foot squared
-units.append( NormalUnit( "mile squared", "mi(les?)? ?(\^2|squared|²)", AREA, 2589990 ) )           #mile squared
+units.append( NormalUnit( "foot squared", "f(oo|ee)?t ?(\^2|squared|²)", AREA, 0.09290304 ) )       #foot squared
+units.append( NormalUnit( "mile squared", "mi(les?)? ?(\^2|squared|²)", AREA, 1609.344**2 ) )       #mile squared
 units.append( NormalUnit( "acre", "acres?", AREA, 4046.8564224 ) )                                  #acre
-units.append( NormalUnit( "rood", "roods?", AREA, 1011.7141 ) )                                     #rood
+units.append( NormalUnit( "rood", "roods?", AREA, 1011.7141056 ) )                                  #rood
 
 #Volume
-units.append( NormalUnit( "pint", "pints?|pt", VOLUME, 0.473176 ) )                     #pint
-units.append( NormalUnit( "quart", "quarts?|qt", VOLUME, 0.946353 ) )                   #quart
-units.append( NormalUnit( "gallon", "gal(lons?)?", VOLUME, 3.78541 ) )                  #gallon
-units.append( NormalUnit( "fluid ounce", "fl\.? oz\.?", VOLUME, 0.0295735296 ) )        #fluid ounce
-units.append( NormalUnit( "teaspoon", "tsp|teaspoons?", VOLUME, 0.00492892159 ) )       #US teaspoon
-units.append( NormalUnit( "tablespoon", "tbsp|tablespoons?", VOLUME, 0.0147867648 ) )   #US tablespoon
-units.append( NormalUnit( "barrel", "drum|barrels?", VOLUME, 119.240471 ) )             #barrel
-units.append( NormalUnit( "peck", "pecks?", VOLUME, 8.809768 ) )                        #pecks
-units.append( NormalUnit( "bushel", "bushels?", VOLUME, 35.23907016688 ) )              #bushels
+units.append( NormalUnit( "pint", "pints?|pt", VOLUME, 0.473176473 ) )                     #pint
+units.append( NormalUnit( "quart", "quarts?|qt", VOLUME, 0.946352946 ) )                   #quart
+units.append( NormalUnit( "gallon", "gal(lons?)?", VOLUME, 3.785411784 ) )                 #gallon
+units.append( NormalUnit( "fluid ounce", "fl\.? oz\.?", VOLUME, 0.0295735295625 ) )        #fluid ounce
+units.append( NormalUnit( "teaspoon", "tsp|teaspoons?", VOLUME, 0.00492892159375 ) )       #US teaspoon
+units.append( NormalUnit( "tablespoon", "tbsp|tablespoons?", VOLUME, 0.01478676478125 ) )  #US tablespoon
+units.append( NormalUnit( "barrel", "drum|barrels?", VOLUME, 158.987294928) )              #barrel
+units.append( NormalUnit( "peck", "pecks?", VOLUME, 8.80976754172 ) )                      #pecks
+units.append( NormalUnit( "bushel", "bushels?", VOLUME, 35.23907016688 ) )                 #bushels
 
 #Energy
-units.append( NormalUnit( "foot-pound", "ft( |\*)?lbf?|foot( |-)pound", ENERGY, 1.355818 ) )                 #foot-pound
-units.append( NormalUnit( "British thermal unit", "btu", ENERGY, 1055.06 ) )                                 #British thermal unit
-units.append( CaseSensitiveUnit( "calories", "cal(ories?)?", ENERGY, 4.184 ) )                               #calories
-units.append( CaseSensitiveUnit( "kilocalories", "(k(ilo)?c|C)al(ories?)?", ENERGY, 4184 ) )       #kilocalories
-units.append( NormalUnit( "ton of refrigeration", "ton of refrigeration", POWER, 3500 ) )                    #ton of refrigeration
+units.append( NormalUnit( "foot-pound", "ft( |\*)?lbf?|foot( |-)pound", ENERGY, 1.3558179483314004 ) )       #foot-pound
+units.append( NormalUnit( "British thermal unit", "btu", ENERGY, 1055.05585262 ) )                           #British thermal unit
+units.append( CaseSensitiveNormalUnit( "calories", "cal(ories?)?", ENERGY, 4.184 ) )                               #calories
+units.append( CaseSensitiveNormalUnit( "kilocalories", "(k(ilo)?c|C)al(ories?)?", ENERGY, 4184 ) )                 #kilocalories
 units.append( NormalUnit( "ergs", "ergs?", ENERGY, 10**-7 ) )                                                #ergs
 
 #Force
-units.append( NormalUnit( "pound-force", "pound( |-)?force|lbf", FORCE, 4.448222 ) )            #pound-force
+units.append( NormalUnit( "pound-force", "pound( |-)?force|lbf", FORCE, 4.4482216152605 ) )            #pound-force
 
 #Torque
-units.append( NormalUnit( "pound-foot", "Pound(-| )?(f(oo|ee)?t)|lbf( |\*)?ft", TORQUE, 1.355818 ) )      #pound-foot
+units.append( NormalUnit( "pound-foot", "Pound(-| )?(f(oo|ee)?t)|lbf( |\*)?ft", TORQUE, 1.3558179483314004 ) )      #pound-foot
 
 #Velocity
 units.append( NormalUnit( "miles per hour", "miles? per hour|mph|mi/h", VELOCITY, 0.44704 ) )             #miles per hour
-units.append( NormalUnit( "knot", "knots?|kts?", VELOCITY, 0.51444444444 ) )                              #knots
+units.append( NormalUnit( "knot", "knots?|kts?", VELOCITY, 1852 / 3600 ) )                              #knots
 units.append( NormalUnit( "feet per second", "f(oo|ee)?t ?(per|/|p) ?s(ec|onds?)?", VELOCITY, 0.3048 ) )  #feet per second
 
 #Temperature
@@ -251,19 +268,19 @@ units.append( NormalUnit( "degrees fahrenheit", "((°|º|deg(ree)?s?) ?)?(fahren
 units.append( NormalUnit( "degrees rankine", "((°|º|deg(ree)?s?) ?)?(ra?(nkine)?)", TEMPERATURE, 5/9, -491.67 ) )          #Degrees rankine
 
 #Pressure
-units.append( NormalUnit( "pound per square inch", "pounds?((-| )?force)? per square in(ch)?|lbf\/in\^2|psi", PRESSURE, 0.068046 ) ) #Pounds per square inch
+units.append( NormalUnit( "pound per square inch", "pounds?((-| )?force)? per square in(ch)?|lbf\/in\^2|psi", PRESSURE, 0.0680459639098777333996809617108008 ) ) #Pounds per square inch
 
 #Mass
-units.append( NormalUnit( "ounce", "ounces?|oz", MASS, 28.349523125 ) )                  #ounces
-units.append( NormalUnit( "pound", "pounds?|lbs?", MASS, 453.59237 ) )                   #pounds
-units.append( NormalUnit( "stone", "stones?|(?<!1)st", MASS, 6350.2293318 ) )            #stones
-units.append( NormalUnit( "grain", "grains?", MASS, 0.06479891 ) )                       #grains
-units.append( NormalUnit( "slug", "slugs?", MASS, 14593.9029 ) )                         #slug
-units.append( NormalUnit( "troy ounce", "troy ?ounces?", MASS, 31.1034768 ) )            #troy ounces
-units.append( NormalUnit( "pennyweight", "penny ?weights?", MASS, 1.55517384 ) )         #pennywheight
-units.append( NormalUnit( "troy pound", "troy ?pounds?", MASS, 373.2417216 ) )           #troy pound
-units.append( NormalUnit( "dram", "drams?", MASS, 1.7718451953125 ) )                    #drams
-units.append( NormalUnit( "hundredweight", "hundredweights?|cwt", MASS, 50802 ) )        #hundredweights
+units.append( NormalUnit( "ounce", "ounces?|oz", MASS, 28.349523125 ) )                     #ounces
+units.append( NormalUnit( "pound", "pounds?|lbs?", MASS, 453.59237 ) )                      #pounds
+units.append( NormalUnit( "stone", "stones?|(?<!1)st", MASS, 6350.29318 ) )                 #stones
+units.append( NormalUnit( "grain", "grains?", MASS, 0.06479891 ) )                          #grains
+units.append( NormalUnit( "slug", "slugs?", MASS, 14593.90293720636482939632545931759 ) )   #slug
+units.append( NormalUnit( "troy ounce", "troy ?ounces?", MASS, 31.1034768 ) )               #troy ounces
+units.append( NormalUnit( "pennyweight", "penny ?weights?", MASS, 1.55517384 ) )            #pennywheight
+units.append( NormalUnit( "troy pound", "troy ?pounds?", MASS, 373.2417216 ) )              #troy pound
+units.append( NormalUnit( "dram", "drams?", MASS, 1.7718451953125 ) )                       #drams
+units.append( NormalUnit( "hundredweight", "hundredweights?|cwt", MASS, 50802.34544 ) )     #hundredweights
 
 #Distance
 units.append( NormalUnit( "inch", "inch(es)?", DISTANCE, 0.0254 ) )                           #inch
@@ -273,7 +290,7 @@ units.append( NormalUnit( "yard", "yd|yards?", DISTANCE, 0.9144 ) )             
 units.append( NormalUnit( "nautical mile", "nautical ?(mi(les?)?)?|nmi", DISTANCE, 1852 ) )   #nautical miles
 units.append( NormalUnit( "thou", "thou", DISTANCE, 0.0000254 ) )                             #thou
 units.append( NormalUnit( "fathom", "fathoms?", DISTANCE, 1.8288 ) )                          #fathom
-units.append( NormalUnit( "furlong", "furlongs?", DISTANCE, 201.1680 ) )                      #furlong
+units.append( NormalUnit( "furlong", "furlongs?", DISTANCE, 201.168 ) )                      #furlong
 units.append( NormalUnit( "rack unit", "rack ?units?|ru", DISTANCE, 0.04445 ) )               #rack units
 units.append( NormalUnit( "smoot", "smoots?", DISTANCE, 1.7018 ) )                            #Smoot units
 
@@ -281,7 +298,45 @@ units.append( NormalUnit( "smoot", "smoots?", DISTANCE, 1.7018 ) )              
 #units.append( NormalUnit( "Lumen", "lumens?|lm", LUMINOUSINTENSITY, 1 ) )          #lumens
 
 #Power
-units.append( NormalUnit( "horsepower", "horse ?power", POWER, 745.699872 ) )         #horsepower
+units.append( NormalUnit( "horsepower", "horse ?power", POWER, 745.69987158227022 ) )         #horsepower
+units.append( NormalUnit( "ton of refrigeration", "ton of refrigeration", POWER, 10550.5585262 / 3 ) )                    #ton of refrigeration
+
+# END UNIT DEFINITIONS
+# BEGIN SUPERUNIT DEFINITIONS
+
+superunits = {}
+for key in units:
+    superunits[key] = []
+    if (key._toSIAddition != 0):
+        continue
+    for potentialsuperunit in units:
+        if (key._unitType != potentialsuperunit._unitType):
+            continue
+        if (key == potentialsuperunit):
+            continue
+        if (potentialsuperunit._toSIAddition != 0):
+            continue
+        ratio = potentialsuperunit._toSIMultiplication / key._toSIMultiplication
+        if (abs(ratio - round(ratio)) > 1.0e-6):
+            continue
+        ratio = round(ratio)
+        if (ratio <= 1):
+            continue
+        if (isinstance(potentialsuperunit, NormalUnit)):
+            superunits[key].append((RegexFlagsExposedNormalUnit(
+                potentialsuperunit._friendlyName,
+                potentialsuperunit._regex.pattern + "(?=$)",
+                potentialsuperunit._regex.flags,
+                potentialsuperunit._unitType,
+                potentialsuperunit._toSIMultiplication,
+                potentialsuperunit._toSIAddition, # this is always zero
+                potentialsuperunit
+            ), ratio))
+        else:
+            print("Superuniting is not supported for " + type(potentialsuperunit) + "!")
+            print("Somebody must've added some code but not fully integrated it *shoves work at you*")
+            print("Or maybe somebody made a class that COULD subclass NormalUnit but just didn't for no reason.")
+            print("That would be nice, it means I just shoved less work at you.")
 
 #Processes a string, converting freedom units to science units.
 def process(message):

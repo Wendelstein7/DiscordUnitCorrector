5e44ce6bf6e5729488f9998eeec636f8eb04497a
Wendelstein7 <23213464+Wendelstein7@users.noreply.github.com>
2023-02-01 23:43:39 +0100

    No statefull class needed

--- filter.py
+++ filter.py
@@ -1,22 +1,21 @@
+def __apply_mention_everyone(text):
+    text = text.replace('@everyone', '@\u200beveryone')
+    text = text.replace('@here', '@\u200bhere')
+    return text
+
+
+def __apply_all_mentions(text):
+    text = text.replace('@', '@\u200b')
+    return text
+
+
 class Filter:
     '''Hasty hot fix module, used to prevent @everyone and @here mentions.'''
 
-    def init(self):
-        pass
-
-    def apply(self, text):
-        text = self.__apply_mention_everyone(text)
+    def apply(text):
+        text = __apply_mention_everyone(text)
         return text
 
-    def apply_strict(self, text):
-        text = self.__apply_all_mentions(text)
-        return text
-
-    def __apply_mention_everyone(self, text):
-        text = text.replace('@everyone', '@\u200beveryone')
-        text = text.replace('@here', '@\u200bhere')
-        return text
-
-    def __apply_all_mentions(self, text):
-        text = text.replace('@', '@\u200b')
+    def apply_strict(text):
+        text = __apply_all_mentions(text)
         return text
